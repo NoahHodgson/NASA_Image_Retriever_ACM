fa1000c252698f96453cacadd117e21e2fefcd8b
Noah Hodgson <noahmhodgson@gmail.com>
2021-03-03 21:21:42 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function updateObject() {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=lTE751UXmg73sa2nd1kAQwZyznTxgkMCaH6gbsW8&count=1').then(res => res.json()).then(
+        res => {
+            let spaceObject = res[0];
+            let title = document.getElementById('image-title')
+            let desc = document.getElementById('image-description')
+            let image = document.getElementById('image')
+
+            title.innerText = spaceObject.title;
+            desc.innerText = spaceObject.explanation;
+            image.src = spaceObject.url;
+            image.alt = spaceObject.title;
+        }
+    )
+}
+
+document.getElementById("new-button").addEventListener('click', updateObject)
+
+window.onload = updateObject;
